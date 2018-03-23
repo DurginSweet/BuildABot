cd9e9b89e0fc9bf2465da503e86b273687cf472a
DurginSweet <Durgin.Sweet@maine.edu>
2018-03-23 18:02:50 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-import Tkinter
+from Tkinter import *
+
+commands = []
+
+Class dev_window:
+  
+  def __init__(self,root):
+    
+    frame = Frame(root)
+    frame.pack
+    
+    in = Entry(root)
+    in.pack
+    
+    read = Button(root,text = "Get code", command = self.readcode,in)
+    read.pack
+    
+    def readcode(self,in):
+      text = in.get()
+      print(text)
+    
+    
+root = Tk("Reddit bot creator")
+
